df05188020a85aa87b9e75ed45f0a972d0a7f0a9
Talipov S.N <proffix4@gmail.com>
2023-11-17 21:24:08 +0600

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,10 @@
+# DialogBlocksConsoleAppHello
+Пример простейшей консольной программы на C++ с использованием wxWidgets и DialogBlocks для Visual Studio 2022
+
+## Ссылки:
+
+http://www.anthemion.co.uk/dialogblocks/
+
+https://www.wxwidgets.org/
+
+https://visualstudio.microsoft.com/ru/vs/community/
---
+++ Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <wx/wx.h>
+
+
+int main(int argc, char** argv)
+{
+	wxLocale m_locale; // Создать объект локали для wxWidgets
+	m_locale.Init(wxLANGUAGE_RUSSIAN, wxLOCALE_DONT_LOAD_DEFAULT); // Установить локаль для wxWidgets
+
+#ifdef __WXMSW__ // Определение для Windows
+	SetConsoleCP(1251); // Установить кодовую страницу ввода для Windows
+	SetConsoleOutputCP(1251); // Установить кодовую страницу вывода для Windows
+#elif __WXGTK__ // Определение для Linux
+	wxPuts(wxT("Замечательно! Das ist großartig! Wonderful! 精彩的！ رائع!")); // Вывести строку
+#endif
+
+	std::wstring input; // Создать строковую переменную
+	wxPuts(wxT("Как вас зовут?")); // Вывести строку
+	std::wcin >> input; // Считать строку
+	wxString result(input); // Создать строку
+	wxPuts(L"Привет," + result + "!"); // Вывести строку
+}
